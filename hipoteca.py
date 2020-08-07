0ca0ea42787b3eb351f1b6348c3c94432f9a4d6e
Valentina Shingaki <valentina.shingaki@gmail.com>
2020-08-07 16:56:11 -0300

    hipoteca final

--- hipoteca.py
+++ hipoteca.py
@@ -16,5 +16,6 @@ while saldo > 0 :
 	saldo = saldo * (1+tasa/12) - pago_actual
 	total_pagado = total_pagado + pago_actual
 	mes = mes + 1
+	print(mes, round(total_pagado,2), round(saldo,2))
 
 print('Total pagado', round(total_pagado, 2), "en: ", mes," meses")
