49c2ad891392198e15d29317e543218ec5af9a44
Valentina Shingaki <valentina.shingaki@gmail.com>
2020-08-07 16:49:14 -0300

    hipoteca V3

--- hipoteca.py
+++ hipoteca.py
@@ -6,14 +6,15 @@ tasa = 0.05
 pago_mensual = 2684.11
 total_pagado = 0.0
 mes=0
+pago_extra_mes_comienzo = 61
+pago_extra_mes_fin = 108
 pago_extra = 1000
 
+
 while saldo > 0 :
-	pago_actual = pago_mensual + (pago_extra if mes<12 else 0)
+	pago_actual = pago_mensual + (pago_extra if mes>=pago_extra_mes_comienzo and mes<=pago_extra_mes_fin else 0)
 	saldo = saldo * (1+tasa/12) - pago_actual
 	total_pagado = total_pagado + pago_actual
-	mes += 1
-	
-            
+	mes = mes + 1
 
 print('Total pagado', round(total_pagado, 2), "en: ", mes," meses")
