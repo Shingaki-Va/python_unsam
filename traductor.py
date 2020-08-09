4b2964412091b133aaf4f0a075640ae325bc1a3f
Valentina Shingaki <valentina.shingaki@gmail.com>
2020-08-09 13:56:35 -0300

    traductor con ingreso por teclado

--- traductor.py
+++ traductor.py
@@ -1,8 +1,30 @@
-frase = 'todos somos programadores'
+print("Bienvenido al Traductor a Lenguage inclusivo")
+print()
+print("Ingrese la Fase a traducir:")
+frase=input()
+
+#frase = 'todos somos programadores'
 palabras = frase.split()
-for palabra in palabras:
-    if ?
-        ...
-frase_t = ?
-print(frase_t)
-'todes somes programdores'
+palabras_inclusive = []
+frase_inclusive = []
+
+for palabras in palabras:
+    if palabras[-1] == "o":
+        for p in palabras:
+            palabras_inclusive.append(p)
+        palabras_inclusive[-1] = "e"
+        palabras_inclusive="".join(palabras_inclusive)
+    elif palabras[-2] == "o":
+        for p in palabras:
+            palabras_inclusive.append(p)
+        palabras_inclusive[-2] = "e"
+        palabras_inclusive="".join(palabras_inclusive)
+    else:
+        for p in palabras:
+            palabras_inclusive.append(p)
+        palabras_inclusive = "".join(palabras_inclusive)
+    frase_inclusive.append(palabras_inclusive)
+    palabras_inclusive = []
+frase_inclusive = " ".join(frase_inclusive)
+
+print(frase_inclusive)
